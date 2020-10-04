c5df6ab4e64d40e4982a39d78bf4d5014a7100f8
slk007 <shubham.soni.lko@gmail.com>
2020-10-04 20:46:45 +0530

    postive test

--- testing.py
+++ testing.py
@@ -21,14 +21,16 @@ class SimpleTest(unittest.TestCase):
     def test_make_book_api_url(self):
 
         url = "https://www.goodreads.com/book/show/12177850.xml?key="+ dev_key
-
+        
+        self.obj.book_id = "12177850"
         self.obj.make_book_api_url()
         book_url = self.obj.book_url
-        print(book_url, url)
+        
         self.assertEqual(book_url, url)
 
     def test_get_book_details(self):
 
+        self.obj.book_url = "https://www.goodreads.com/book/show/12177850.xml?key="+ dev_key
         self.obj.get_book_details()
 
         self.assertEqual(self.obj.book_details['title'], "A Song of Ice and Fire (A Song of Ice and Fire, #1-5)")
